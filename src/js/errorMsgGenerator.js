62a8aef43209a3e7f222e26789fc099b7f3b6b17
Katerina Fedorets <katia.fedorets@gmail.com>
2020-11-15 15:35:41 +0200

    all

---
+++ src/js/errorMsgGenerator.js
@@ -0,0 +1,26 @@
+const errorBox = document.querySelector(".form__error-box");
+import { alert } from "@pnotify/core";
+import "@pnotify/core/dist/BrightTheme.css";
+import "@pnotify/core/dist/PNotify.css";
+import getRefs from "./getRefs.js";
+const refs = getRefs();
+
+// дабовляет ошибку на страницу, если ее еще на странице нет
+function errorMsgMarkUp() {
+    const errorMsg = document.querySelector(".pnotify");
+    if (!errorMsg) {
+        refs.countriesList.innerHTML = "";
+        alert(`Too many matches found.
+    Please enter a more specific query!`);
+    }
+}
+
+// удаляет ошибку при создании элемента страны
+function hideError() {
+    const errorMsg = document.querySelector(".pnotify-container");
+    if (errorMsg) {
+        errorMsg.classList.add("hidden");
+    }
+}
+
+export default { errorMsgMarkUp, hideError };
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,9 @@
+function fetchCountries(searchQuery) {
+    const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
+
+    return fetch(url)
+        .then(resp => resp.json())
+        .then(data => data);
+}
+export default fetchCountries;
+
